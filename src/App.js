c189576bf5456d9cadf569e09a7fd37915347f1b
Arifulhaque-02128 <arifulhaque02128@gmail.com>
2020-10-05 23:09:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import Header from './Components/Header/Header';
+import Login from './Components/Login/Login';
+import Registration from './Components/Registration/Registration';
+import { createContext } from 'react';
+import { useState } from 'react';
+import RegistrationHistoryForUser from './Components/RegistrationHistoryForUser/RegistrationHistoryForUser';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
 
 function App() {
+  const [userInfo, setUserInfo] = useState({
+    isLoggedIn : false,
+    userName : '',
+    email : '',
+    eventName : '',
+    eventImg : '',
+    errorMessage : ''
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[userInfo, setUserInfo]}>
+      <Router>
+        <Switch>
+          <Route path='/home'>
+            <Header></Header>
+            <Home></Home>
+          </Route>
+          <PrivateRoute path='/register'>
+            <Header></Header>
+            <Registration></Registration>
+          </PrivateRoute>
+          <Route path='/login'>
+            <Header></Header>
+            <Login></Login>
+          </Route>
+          <PrivateRoute path="/userHistory">
+            <Header></Header>
+            <RegistrationHistoryForUser></RegistrationHistoryForUser>
+          </PrivateRoute>
+          <Route exact path='/'>
+            <Header></Header>
+            <Home></Home>
+          </Route>
+          <Route path='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
